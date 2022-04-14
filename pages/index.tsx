bf19dfe945d54866ff2cbbc8bd41c9a813edbf79
Andrei Bledea <andrei.a.bledea@gmail.com>
2022-04-14 12:17:51 +0300

    basic UI structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,15 @@ import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
-     <div className="text-2xl text-red-200">Sup Boys</div>
+    <div className="h-screen w-screen flex flex-col items-center justify-center">
+      <div className="text-2xl text-center">Witch poke is Roundest??</div>
+      <div className="p-2"/>
+      <div className="border rounded p-8 flex justify-between items-center max-w-2xl">
+        <div className="w-16 h-16 bg-red-200"></div>
+        <div className="p-8">VS</div>
+        <div className="w-16 h-16 bg-red-200"></div>
+      </div>
+     </div>
   )
 }
 
