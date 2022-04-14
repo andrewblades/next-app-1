8d6f31f2e68938548a57b123a8ff68167947a9a9
Andrei Bledea <andrei.a.bledea@gmail.com>
2022-04-14 13:23:12 +0300

    changed package.json because of version errors, added trpc, moving forward

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,13 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import {trpc} from '@/utils/trpc'
 
 const Home: NextPage = () => {
+  const { data, isLoading } = trpc.useQuery(["hello", { text: "Andrei" }])
+  
+  if (isLoading) return <div>Loading....</div>
+  if (data) return <div>{data.greeting}</div>
   return (
     <div className="h-screen w-screen flex flex-col items-center justify-center">
       <div className="text-2xl text-center">Witch poke is Roundest??</div>
