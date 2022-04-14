a7c7bd40d4fdf95779e992cf6bdf67fd7fe940f8
Andrei Bledea <andrei.a.bledea@gmail.com>
2022-04-14 13:33:17 +0300

    added random pokemon id fetcher

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,20 +2,20 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import {trpc} from '@/utils/trpc'
+import { getOptionsForVote } from '@/utils/getRandomPokemon'
 
 const Home: NextPage = () => {
-  const { data, isLoading } = trpc.useQuery(["hello", { text: "Andrei" }])
   
-  if (isLoading) return <div>Loading....</div>
-  if (data) return <div>{data.greeting}</div>
+  const [first, second] = getOptionsForVote()
+
   return (
     <div className="h-screen w-screen flex flex-col items-center justify-center">
       <div className="text-2xl text-center">Witch poke is Roundest??</div>
       <div className="p-2"/>
       <div className="border rounded p-8 flex justify-between items-center max-w-2xl">
-        <div className="w-16 h-16 bg-red-200"></div>
+        <div className="w-16 h-16 bg-red-800">{first}</div>
         <div className="p-8">VS</div>
-        <div className="w-16 h-16 bg-red-200"></div>
+        <div className="w-16 h-16 bg-red-800">{second}</div>
       </div>
      </div>
   )
