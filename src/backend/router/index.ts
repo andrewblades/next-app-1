111fb4c8da7bf2ac3a84a580ded895269bf070ff
Andrei Bledea <andrei.a.bledea@gmail.com>
2022-04-15 11:34:35 +0300

    Add prisma and basic endpoint for voting

--- src/backend/router/index.ts
+++ src/backend/router/index.ts
@@ -1,6 +1,6 @@
 import * as trpc from '@trpc/server';
 import { z } from 'zod';
-
+import { prisma } from '@/backend/utils/prisma'
 import { PokemonClient } from 'pokenode-ts';
 
 
@@ -11,6 +11,19 @@ export const appRouter = trpc.router().query("get-pokemon-by-id", {
         const api = new PokemonClient();
         const pokemon = await api.getPokemonById(input.id);
         return { name: pokemon.name, sprites: pokemon.sprites }
+    }
+}).mutation("cast-vote", {
+    input: z.object({
+        votedFor: z.number(),
+        votedAgainst: z.number()
+    }),
+    async resolve({input}) {
+        const voteInDb = await prisma.vote.create({
+            data: {
+              ...input
+            }
+        })
+        return {success: true, vote: voteInDb}
     }
 })
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,18 +3,27 @@ import Head from 'next/head'
 import Image from 'next/image'
 import {trpc} from '@/utils/trpc'
 import { getOptionsForVote } from '@/utils/getRandomPokemon'
-import React from 'react'
+import { inferQueryResponse } from './api/trpc/[trpc]'
+import type React from 'react'
+import {useState} from 'react'
+
+const btnStyle = "inline-flex items-center px-2.5 py-1.5 border border-gray-300 shadow-sm text-xs font-medium rounded text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
 
 const Home: NextPage = () => {
   
-  const [ids, updateIds] = React.useState(() => getOptionsForVote())
+  const [ids, updateIds] = useState(() => getOptionsForVote())
 
   const [first, second] = ids
   
   const firstPokemon = trpc.useQuery(["get-pokemon-by-id", {id: first}])
   const secondPokemon = trpc.useQuery(["get-pokemon-by-id", { id: second }])
   
-  if(firstPokemon.isLoading || secondPokemon.isLoading) return null
+  if (firstPokemon.isLoading || secondPokemon.isLoading) return null
+  
+  const voteForRoundest = (selected: number) => {
+    // todo: fire mutation to persist changes
+    updateIds(getOptionsForVote())
+  }
   
   console.log(firstPokemon.data)
 
@@ -23,19 +32,16 @@ const Home: NextPage = () => {
       <div className="text-2xl text-center">Witch poke is Roundest??</div>
       <div className="p-2"/>
       <div className="border rounded p-8 flex justify-between items-center max-w-2xl">
-        <div className="w-64 h-64 flex flex-col">
-          <img
-            src={firstPokemon.data?.sprites.front_default}
-            className="w-full" />
-          <div className="text-xl text-center capitalize mt-[-2rem]">{firstPokemon.data?.name}</div>  
-        </div>
-        <div className="p-8">VS</div>
-        <div className="w-64 h-64 flex flex-col">
-          <img
-            src={secondPokemon.data?.sprites.front_default}
-            className="w-full" />
-          <div className="text-xl text-center capitalize mt-[-2rem]">{secondPokemon.data?.name}</div>  
-        </div>
+        {!firstPokemon.isLoading &&
+          firstPokemon.data &&
+          !secondPokemon.isLoading &&
+          secondPokemon.data && (
+          <>
+            <PokemonListing pokemon={firstPokemon.data} vote={() => voteForRoundest(first)} />
+            <div className="p-8">VS</div>
+             <PokemonListing pokemon={secondPokemon.data} vote={() => voteForRoundest(second)} />
+          </>
+        )}
         <div className="p-2"></div>
       </div>
      </div>
@@ -43,3 +49,22 @@ const Home: NextPage = () => {
 }
 
 export default Home
+
+type PokemonFromServer = inferQueryResponse<"get-pokemon-by-id">;
+
+const PokemonListing: React.FC<{
+  pokemon: PokemonFromServer,
+  vote: () => void
+}> = (props) => {
+  return (
+      <div className="flex flex-col items-center">
+          <img
+            src={props.pokemon.sprites.front_default}
+            className="w-64 h-64 " />
+          <div className="text-xl text-center capitalize mt-[-2rem]">
+            {props.pokemon.name}
+          </div>
+          <button className={btnStyle} onClick={() => props.vote()}>Rounder</button>
+        </div>
+   )
+}
